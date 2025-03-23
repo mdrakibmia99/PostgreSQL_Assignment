272f6421a168bcd0922bcc49fe4f0053b8047964
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-03-23 23:21:50 +0600

    create books , customer, order table and inserted data done

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,1 +1,62 @@
-PostgreSQL_Assignment 
+-- Active: 1742151109658@@127.0.0.1@5432@bookstore_db
+-- create database bookstore_db
+CREATE DATABASE bookstore_db;
+
+-- create 'books' table   
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(200) NOT NULL,
+    author VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) CHECK (price >= 0) NOT NULL,
+    stock INT CHECK (stock >= 0) NOT NULL,
+    published_year INT CHECK (published_year > 0) NOT NULL
+);
+
+-- insert into books table
+INSERT INTO books (title, author, price, stock, published_year) VALUES
+('The Pragmatic Programmer', 'Andrew Hunt', 40.00, 10, 1999),
+('Clean Code', 'Robert C. Martin', 35.00, 5, 2008),
+('You Don''t Know JS', 'Kyle Simpson', 30.00, 8, 2014),
+('Refactoring', 'Martin Fowler', 50.00, 3, 1999),
+('Database Design Principles', 'Jane Smith', 20.00, 0, 2018);
+
+
+
+
+-- create "customers" table
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(200) NOT NULL,
+    email VARCHAR(200) UNIQUE NOT NULL,
+    joined_date DATE DEFAULT CURRENT_DATE
+);
+
+-- insert into customers table
+INSERT INTO customers (name, email, joined_date) VALUES
+('Alice', 'alice@email.com', '2023-01-10'),
+('Bob', 'bob@email.com', '2022-05-15'),
+('Charlie', 'charlie@email.com', '2023-06-20');
+
+
+
+-- create 'orders' table 
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(id) ON DELETE CASCADE,
+    book_id INT REFERENCES books(id) ON DELETE CASCADE,
+    quantity INT CHECK (quantity > 0) NOT NULL,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- insert into orders table
+INSERT INTO orders (customer_id, book_id, quantity, order_date) VALUES
+(1, 2, 1, '2024-03-10'),
+(2, 1, 1, '2024-02-20'),
+(1, 3, 2, '2024-03-05');
+
+
+
+select * from books;
+select * from customers;    
+
+select * from orders;
